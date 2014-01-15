e3cdbd527ad854129e0195ed8d8a153afdd17aaf
Josh Owens <joshua.owens@gmail.com>
2014-01-15 16:19:10 -0500

    Add text field and DRY up a bunch of code shared across some methods.

--- helper.js
+++ helper.js
@@ -1,3 +1,41 @@
+processClass = function(optionsHash) {
+  if (optionsHash['class']) {
+    html_class = " " + optionsHash['class']
+  } else {
+    html_class = ""
+  }
+  return html_class
+}
+
+processLabel = function(optionsHash, field) {
+  if (optionsHash['label']) {
+    label_words = optionsHash['label']
+  } else {
+    label_words = _.humanize(field)
+  }
+  return label_words
+}
+
+buildLabel = function(optionsHash, field) {
+  label_words = processLabel(optionsHash, field)
+  return "<label for='"+ field +"'>" + label_words + "</label>"
+}
+
+/*----- HELPERS ------*/
+
+Handlebars.registerHelper('text_field', function(field, options){
+  var _this = this;
+  if (!field) {
+    return;
+  }
+  value = _this[field] || ""
+  html_class = processClass(options.hash)
+  html = "<input type='text' id='" + field + "' name='"+ field +"' value='"+ value +"' class='form-control"+ html_class +"'>"
+  label = buildLabel(options.hash, field)
+  return new Handlebars.SafeString(label + html);
+});
+
+
 Handlebars.registerHelper('select_box', function(field, options) {
   var html_options,
   _this = this;
@@ -5,17 +43,14 @@ Handlebars.registerHelper('select_box', function(field, options) {
     return;
   }
 
-  if (options.hash['class']) {
-    html_class = " " + options.hash['class']
-  } else {
-    html_class = ""
-  }
+  html_class = processClass(options.hash)
 
   if (options.hash.optionValues && options.hash.optionValues.length > 0) {
     optionsValues = options.hash.optionValues
   } else {
     optionsValues = _this["" + field + "Options"]();
   }
+
   html_options = [];
   _.each(optionsValues, function(option) {
     var selected;
@@ -23,15 +58,19 @@ Handlebars.registerHelper('select_box', function(field, options) {
     return html_options.push("<option value='" + option + "'" + selected + ">" + _.humanize(option) + "</option>");
   });
   html = "<select class='form-control" + html_class + "' name='" + field + "'>" + (html_options.join('')) + "</select>"
-  return new Handlebars.SafeString(html);
+  label = buildLabel(options.hash, field)
+  return new Handlebars.SafeString(label + html);
 });
+
 
 Handlebars.registerHelper('check_box', function(field) {
   var capitalizedField, checked;
   if (!field) {
     return;
   }
+  html_class = processClass(options.hash)
   checked = this[field] === 'true' ? ' checked' : '';
-  html = "<label><input name='" + field + "' type='hidden' value='false'><input name='" + field + "' type='checkbox' value='true' " + checked + ">" + _.humanize(field) + "</label>";
+  label = processLabel(options.hash, field)
+  html = "<label for='"+ field +"'><input id='"+ field +"' name='" + field + "' type='hidden' value='false'><input name='" + field + "' class='"+ html_class +"' type='checkbox' value='true' " + checked + ">" + label + "</label>";
   return new Handlebars.SafeString(html);
 });
