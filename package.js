49817f82e730de93eced9f7b1d3080974e185357
Josh Owens <joshua.owens@gmail.com>
2014-06-03 16:54:14 -0400

    Include dependencies we need.

--- package.js
+++ package.js
@@ -3,7 +3,7 @@ Package.describe({
 });
 
 Package.on_use(function(api) {
-  api.use(['ui', 'underscore'], 'client');
+  api.use(['ui', 'underscore', 'underscore-string-latest'], 'client');
   api.add_files(['helper.js', 'simpleform.js'], 'client');
   api.export('SimpleForm', 'client');
 });
