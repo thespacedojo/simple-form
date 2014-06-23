415283df846623243d4bc1f295a8ad6ddd91dda1
Josh Owens <joshua.owens@gmail.com>
2014-06-23 15:36:52 -0400

    Don't set empty dates and update version

--- simpleform.js
+++ simpleform.js
@@ -4,7 +4,7 @@ SimpleForm = {
     array = $(target).serializeArray();
     _.each(array, function(formItem) {
       type = $(target).find("input[name='" + formItem.name + "']").attr('type')
-      if (type === 'date') {
+      if (type === 'date' && !!formItem.value) {
         return form[formItem.name] = new Date(formItem.value + " 00:00");
       } else {
         return form[formItem.name] = formItem.value;
