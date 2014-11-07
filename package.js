f81347d9fb7f1ea6c75aaf17bc6f8d5bf5851236
Josh Owens <joshua.owens@gmail.com>
2014-11-07 11:26:30 -0500

    Add uploaded as a dep.

--- package.js
+++ package.js
@@ -1,13 +1,13 @@
 Package.describe({
   summary: "A meteorite package that makes building dynamic two way forms easy",
-  version: '0.2.1',
+  version: '0.2.2',
   name: "joshowens:simple-form",
   git: 'https://github.com/MeteorClub/simple-form'
 });
 
 Package.onUse(function(api) {
   api.versionsFrom("METEOR@0.9.0");
-  api.use(['ui', 'templating', 'underscore', 'mrt:underscore-string-latest@2.3.3'], 'client');
+  api.use(['ui', 'templating', 'underscore', 'schnie:uploader', 'mrt:underscore-string-latest@2.3.3'], 'client');
   api.addFiles(['fileField.html', 'fileField.js', 'helper.js', 'simpleform.js'], 'client');
   api.export('SimpleForm', 'client');
 });
